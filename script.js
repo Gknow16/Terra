5a4a2ccef32021129a5f96ad192d2e9ab4f2809b
Gknow16 <gmabangue16@gmail.com>
2022-10-24 21:59:00 +0800

    added

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
